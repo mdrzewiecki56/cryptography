2ea7f257972b6df6217bf38bc33142e130a81cf3
mdrzewiecki <mdrzewiecki56@gmail.com>
2019-10-19 17:21:24 +0200

    Updates

--- precache-manifest.a9a925028c27559fbe7b752616502326.js
+++ precache-manifest.a9a925028c27559fbe7b752616502326.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "636a6c76b0fce5ea6f10373039a7a2bf",
+    "revision": "c0bed73b688de9755b16205b971b9769",
     "url": "/cryptography/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cryptography/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "b1a84d097bfb0e107895",
+    "revision": "8bdca78a76ec188c1128",
     "url": "/cryptography/static/css/main.a8a9ecc4.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cryptography/static/js/2.f83275af.chunk.js"
   },
   {
-    "revision": "b1a84d097bfb0e107895",
-    "url": "/cryptography/static/js/main.892c8173.chunk.js"
+    "revision": "8bdca78a76ec188c1128",
+    "url": "/cryptography/static/js/main.cf441307.chunk.js"
   },
   {
     "revision": "229214ab573927bc1fa7",
